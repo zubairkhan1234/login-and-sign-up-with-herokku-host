bd8b225a7db92116e455ed32648ea22706b4d64e
zubairkhan12341 <zubairsaleem1210@gmail.com>
2020-12-24 16:56:37 +0500

    some work

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('dev'));
-const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res, next) => {
     console.log("some one get menu");
